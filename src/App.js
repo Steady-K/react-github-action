9817cf5d172b89bf664bfba0f8803290436e1edf
Steady-K <tkdlxm56@gmail.com>
2025-03-18 01:42:43 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setcount] = useState(0);
+  const [disabled, setdisabled] = useState(false);
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h3 data-testid='counter'>{count}</h3>
+        <button
+          data-testid='minus-button'
+          onClick={() => setcount((prev) => prev - 1)}
+          disabled={disabled}
+        >
+          -
+        </button>
+        <button
+          data-testid='plus-button'
+          onClick={() => setcount((prev) => prev + 1)}
+          disabled={disabled}
+        >
+          +
+        </button>
+        <button
+          style={{ backgroundColor: "blue" }}
+          data-testid='on/off-button'
+          onClick={() => setdisabled((prev) => !prev)}
+        ></button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
